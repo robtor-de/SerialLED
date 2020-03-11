3a64dff4fe474b81f6846493d3db513e66fd8350
Robin Dietzel <robtor2000@web.de>
2020-03-11 19:22:38 +0100

    FEATURE: Added Serial fading command

--- FastLedInterface.h
+++ FastLedInterface.h
@@ -12,9 +12,34 @@ void hardwareSetup() {
   FastLED.show();
 }
 
+//Instantly set the Color
 void setColor(short r, short g, short b) {
   leds[0].r = r;
   leds[0].g = g;
   leds[0].b = b;
   FastLED.show();
+}
+
+//Fade to other color within duration
+void fadeTo(short r, short g, short b, int duration) {
+  short diff[3];
+  float scale[3], dur_step;
+  diff[0] = r - leds[0].r;
+  diff[1] = r - leds[0].r;
+  diff[2] = r - leds[0].r;
+
+  scale[0] = scale8(1, diff[0]);
+  scale[1] = scale8(1, diff[1]);
+  scale[2] = scale8(1, diff[2]);
+
+  dur_step = duration*1000/256.0;
+
+  for(int i = 0; i < 256; i++) {
+    leds[0].r += scale[0];
+    leds[0].g += scale[1];
+    leds[0].b += scale[2];
+    FastLED.show();
+    delay(dur_step);
+  }
+
 }
--- SerialInterface.h
+++ SerialInterface.h
@@ -16,6 +16,7 @@ void serialEvent() {
           //Now switch between different modes
           switch(data[0]) {
             case B00000001: setColor(data[1], data[2], data[3]); Serial.write(0); break;
+            case B00000010: fadeTo(data[1], data[2], data[3], Serial.read()); Serial.write(0); break;
           }
         } else {
           //return 1 as error code when check failed
