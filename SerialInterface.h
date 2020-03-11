0d88fc2703e85076d1eb063f0e3c94602e79dfd2
Robin Dietzel <robtor2000@web.de>
2020-03-11 19:06:59 +0100

    FEATURE: Implemented set color mode via serial

--- SerialInterface.h
+++ SerialInterface.h
@@ -3,13 +3,24 @@ void serialSetup() {
 }
 
 //Is called, when Data is available
-
 void serialEvent() {
   byte data[5];
-  while(Serial.available() > 0) {
+  while(Serial.available() > 0)
+    //Check for start byte
     if(Serial.read() == B11111111) {
-      Serial.write(1);
       Serial.readBytes(data, 5);
+      Serial.flush();
+
+        //Check if packet is complete
+        if(data[4] == (data[1]^data[2]^data[3])) {
+          //Now switch between different modes
+          switch(data[0]) {
+            case B00000001: setColor(data[1], data[2], data[3]); Serial.write(0); break;
+          }
+        } else {
+          //return 1 as error code when check failed
+          Serial.write(1);
+        }
       }
   }
 }
