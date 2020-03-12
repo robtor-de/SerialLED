3cf2a0993d908af9a337f15793f29053eaa420a4
Robin Dietzel <robtor2000@web.de>
2020-03-12 10:40:50 +0100

    BUGIFX: changed overflow detection for dimming

--- FastLedInterface.h
+++ FastLedInterface.h
@@ -23,28 +23,26 @@ void setColor(short r, short g, short b) {
 //Fade to other color within duration
 void fadeTo(short r, short g, short b, int duration) {
   short diff[3];
-  float scale[3], dur_step;
+  uint8_t scale[3], dur_step;
   diff[0] = r - leds[0].r;
   diff[1] = g - leds[0].g;
   diff[2] = b - leds[0].b;
 
 
 
-  scale[0] = scale8_video_LEAVING_R1_DIRTY(1, diff[0]);
-  scale[1] = scale8_video_LEAVING_R1_DIRTY(1, diff[1]);
-  scale[2] = scale8_video_LEAVING_R1_DIRTY(1, diff[2]);
-  cleanup_R1();
+  scale[0] = scale8_video(1, diff[0]);
+  scale[1] = scale8_video(1, diff[1]);
+  scale[2] = scale8_video(1, diff[2]);
 
   dur_step = duration*1000/256.0;
 
+  Serial.write(leds[0].r);
+
   for(int i = 0; i < 256; i++) {
     //if is necessary to prevent overflow
-    if(leds[0].r < 255 && leds[0].r > 0)
-      (diff[0] >= 0)? leds[0].r += scale[0] : leds[0].r -= scale[0];
-    if(leds[0].g < 255 && leds[0].g > 0)
-      (diff[1] >= 0)? leds[0].g += scale[1] : leds[0].g -= scale[1];
-    if(leds[0].b < 255 && leds[0].b > 0)
-      (diff[2] >= 0)? leds[0].b += scale[2] : leds[0].b -= scale[2];
+    (diff[0] >= 0)? if(leds[0].r < 255) leds[0].r += scale[0] : if(leds[0].r > 0) leds[0].r -= scale[0];
+    (diff[1] >= 0)? if(leds[0].g < 255) leds[0].g += scale[1] : if(leds[0].g > 0) leds[0].g -= scale[1];
+    (diff[2] >= 0)? if(leds[0].b < 255) leds[0].b += scale[2] : if(leds[0].b > 0) leds[0].b -= scale[2];
     FastLED.show();
     delay(dur_step);
   }
