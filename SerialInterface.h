4c2d4668b958caa9c251f924e390b460f3c3dc0f
Robin Dietzel <robtor2000@web.de>
2020-03-12 10:09:47 +0100

    BUGFIX: changed data[401] typo to data[4]

--- SerialInterface.h
+++ SerialInterface.h
@@ -14,7 +14,7 @@ void serialEvent() {
           //Now switch between different modes
           switch(data[0]) {
             case B00000001: setColor(data[1], data[2], data[3]); Serial.write(0); break;
-            case B00000010: fadeTo(data[1], data[2], data[3], data[401]); Serial.write(0); break;
+            case B00000010: fadeTo(data[1], data[2], data[3], data[4]); Serial.write(0); break;
           }
         } else {
           //return 1 as error code when check failed
